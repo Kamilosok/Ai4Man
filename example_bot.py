c2e3b3545a84d834859574d36271b490d14e4aae
Kamilosok <kazdancewicz@gmail.com>
2022-09-26 19:42:44 +0200

    Scraping done
    The bot is now able to scrape messages from channels, now we need to save this into a database and use it in ML

--- example_bot.py
+++ example_bot.py
@@ -1,7 +1,6 @@
 import discord
 import token_decoder
 
-#Idk
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
@@ -9,10 +8,27 @@ client = discord.Client(intents=intents)
 decoder = token_decoder.Tokener()
 token = decoder.accessFile(decoder)
 
+#TestID 1021492127647154208
+#GoodId 751861351378583562
+channelId = int(751861351378583562)
 
-#Launching bot
+
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    #Saving channel and server for something
+    channel = client.get_channel(int(channelId))
+    guild = channel.guild
+    counter = 0
+    #If the message is not empty, print it into console with it's author
+    async for message in channel.history(limit = 2147483647, oldest_first = True):
+        if message.content != '':
+            #Change this to save into database
+            print(str(message.author))
+            print(str(message.content) + '\n######################')
+            counter +=1
+            print(counter)
 
-client.run(token)
+if __name__ == "__main__":
+    client.run(token)

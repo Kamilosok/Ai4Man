ea8829737c1c4c6b684f98cb3f7caa6f87ace76f
Kamilosok <kazdancewicz@gmail.com>
2022-10-13 20:42:30 +0200

    Database done
    Saving logs to database works, will begin working on the processing soon

--- example_bot.py
+++ example_bot.py
@@ -1,5 +1,6 @@
 import discord
 import token_decoder
+import pyodbc
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -11,7 +12,11 @@ token = decoder.accessFile(decoder)
 #TestID 1021492127647154208
 #GoodId 751861351378583562
 channelId = int(751861351378583562)
-
+notCheckStart = (':', '!', '*')
+notCheckOverall = ('//', '<@')
+conn = pyodbc.connect(
+        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=F:\Git\Ai4Man\Database\Logs.accdb;')
+cursor = conn.cursor()
 
 
 @client.event
@@ -21,14 +26,23 @@ async def on_ready():
     channel = client.get_channel(int(channelId))
     guild = channel.guild
     counter = 0
-    #If the message is not empty, print it into console with it's author
+    #If the message is not empty or unimportant, save it into the database
     async for message in channel.history(limit = 2147483647, oldest_first = True):
-        if message.content != '':
+        if message.content != '' and not any(x in message.content[0] for x in notCheckStart) and \
+                not any(x in message.content for x in notCheckOverall) and not message.author.bot:
             #Change this to save into database
-            print(str(message.author))
-            print(str(message.content) + '\n######################')
+            #print(str(message.author))
+            out = message.content.lower()
+            insertToDatabase(str(message.author)[0: len(str(message.author))-5], out)
+            #print(str(out) + '\n######################')
             counter +=1
             print(counter)
+
+def insertToDatabase(author, message):
+    sql = "insert into Messages (Author, Message) values (?, ?)"
+    tuples = [(str(author), str(message))]
+    cursor.executemany(sql, tuples)
+    cursor.commit()
 
 if __name__ == "__main__":
     client.run(token)
